362e78e838e2d9d3ae191f5583c3c6d9f5fbfc70
shahabas07 <141425085+shahabas07@users.noreply.github.com>
2023-08-21 16:34:21 +0530

    Add files via upload

---
+++ canvas board/canvas.js
@@ -0,0 +1,50 @@
+var canvas = document.getElementById("can");
+var ctx = canvas.getContext("2d");
+var strokew = document.getElementById("strokew");
+var strokec = document.getElementById("colorbox");
+var stroke = document.getElementById("stroke");
+var x = 0;
+var y = 0;
+
+var isdrawing = false;
+function stopdraw() {
+  isdrawing = false;
+
+}
+
+function startdraw(e) {
+  isdrawing = true;
+  x = e.offsetX;
+  y = e.offsetY;
+}
+
+function draw(e) {
+  if (isdrawing == true) {
+    const newX = e.offsetX;
+    const newY = e.offsetY;
+
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(newX, newY)
+    ctx.stroke();
+
+    x = newX
+    y = newY
+
+  }
+}
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown" , startdraw);
+canvas.addEventListener("mouseup",stopdraw)
+
+strokew.addEventListener("input", function fun(){
+   stroke.innerText = strokew.value
+   ctx.lineWidth = strokew.value;
+  })
+
+  strokec.addEventListener("input",function fun(){
+    ctx.strokeStyle = strokec.value;
+  })
+
+
+
